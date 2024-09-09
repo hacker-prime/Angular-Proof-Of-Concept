88f0013a17445c4b8145c21212850e0d8bc32358
hacker-prime <hacker0prime@gmail.com>
2024-09-08 19:15:13 -0500

    SalaryFormComponent & SalaryListComponent Missing from Declarations
    Since the command in the terminal did not automatically create it, I have to add it manually.

--- salary-app/src/app/salary/salary.module.ts
+++ salary-app/src/app/salary/salary.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SalaryFormComponent } from '../salary-form/salary-form.component';
+import { SalaryListComponent } from '../salary-list/salary-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SalaryFormComponent,
+    SalaryListComponent
+  ],
   imports: [
     CommonModule
   ]
