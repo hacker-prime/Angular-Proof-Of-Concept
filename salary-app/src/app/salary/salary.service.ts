59b5a123023581c643c65c610871bcae96216613
hacker-prime <hacker0prime@gmail.com>
2024-09-10 22:40:01 -0500

    Update Salary Service With CRUD Operations

--- salary-app/src/app/salary/salary.service.ts
+++ salary-app/src/app/salary/salary.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Salary } from '../models/salary';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalaryService {
 
-  constructor() { }
+  private salaries: Salary[] = [];
+
+  getSalaries(): Salary[] {
+    return this.salaries;
+  }
+
+  getSalary(id: string): Salary | undefined {
+    return this.salaries.find(res => res.id === id);
+  }
+
+  addSalary(Salary: Salary): void {
+    this.salaries.push(Salary);
+  }  
+
+  deleteSalary(id: string): void {
+    let index = this.salaries.findIndex(res => res.id === id);
+    this.salaries.splice(index,1)
+  } 
+
+  updateSalary(updatedSalary: Salary): void {
+    let index = this.salaries.findIndex(res => res.id === updatedSalary.id)
+    this.salaries[index] = updatedSalary;
+  }
+
+
+
+
 }
+
+
