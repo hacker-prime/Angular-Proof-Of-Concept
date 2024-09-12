c789a21fc98cc7a8182db59e2b522df93f4c4719
hacker-prime <hacker0prime@gmail.com>
2024-09-11 21:47:01 -0500

    Showing All Salaries In A List

--- salary-app/src/app/salary-list/salary-list.component.ts
+++ salary-app/src/app/salary-list/salary-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SalaryService } from '../salary/salary.service';
+import { Salary } from '../models/salary';
 
 @Component({
   selector: 'app-salary-list',
   templateUrl: './salary-list.component.html',
   styleUrl: './salary-list.component.css'
 })
-export class SalaryListComponent {
+export class SalaryListComponent implements OnInit {
+  
+  salaries: Salary[] = [];
+
+  constructor(private salaryService: SalaryService){
+
+  }
+  
+  ngOnInit(): void {
+
+    this.salaries = this.salaryService.getSalaries();
+
+  }
+
+
 
 }
