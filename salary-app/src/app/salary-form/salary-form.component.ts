948e3f396cfa169ffc4d5357c93795c150730150
hacker-prime <hacker0prime@gmail.com>
2024-09-11 22:14:24 -0500

    Using The Router Navigate Method To Redirect The User
    So whenever you submit the form and you add something, you will get redirected to the salary list.

--- salary-app/src/app/salary-form/salary-form.component.ts
+++ salary-app/src/app/salary-form/salary-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SalaryService } from '../salary/salary.service';
 import { Salary } from '../models/salary';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-salary-form',
@@ -14,7 +15,9 @@ export class SalaryFormComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private salaryService: SalaryService){
+    private salaryService: SalaryService,
+    private router: Router
+  ){
 
   }
 
@@ -33,6 +36,8 @@ export class SalaryFormComponent implements OnInit {
       // console.log("valid")
       let salary: Salary = this.salaryForm.value;
       this.salaryService.addSalary(salary) 
+      //this is how you can navigate/redirect the user inside the applicaton from one page to the next
+      this.router.navigate(['/list'])
     }
   }
 
